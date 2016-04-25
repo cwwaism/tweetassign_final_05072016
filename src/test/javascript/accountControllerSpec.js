3fcac82c03e3ee3b42716b0d99195d3f09be126b
Nayna Nayate <nansak1ster@gmail.com>
2016-04-25 05:49:16 -0500

    Working on r3 jasmine test

---
+++ src/test/javascript/accountControllerSpec.js
@@ -0,0 +1,17 @@
+/*describe('accountController', function())
+{
+    beforeEach(module('app'));
+
+    var $controller
+    var $rootScope
+    var $scope
+
+    var currentUser = undefined;
+
+    var authService = {
+        currentUser: function(){
+            return currentUser;
+
+        }
+    }
+}*/
