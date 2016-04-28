123f97ec0fa64bf8ae6e67d4c89fb6e7761df98d
Nayna Nayate <nansak1ster@gmail.com>
2016-04-28 05:52:37 -0500

    Karma works - need to write R3

--- src/test/javascript/accountControllerSpec.js
+++ src/test/javascript/accountControllerSpec.js
@@ -1,12 +1,16 @@
 describe('accountController', function()
 {
     beforeEach(module('app'));
+    var $controller;
+    var $rootScope;
+    var $scope;
 
     var currentUser = undefined;
+    //postMessage: function () {
 
-
-
-
+    //}
 
 
 });
+
+
