302f51612421a61584b14340e253f98cef1687fd
nansak1 <nansak1ster@gmail.com>
2016-04-28 15:06:43 -0500

    Working on R3 - Jasmine test, fixed some UI stuff

--- src/test/javascript/accountControllerSpec.js
+++ src/test/javascript/accountControllerSpec.js
@@ -1,14 +1,41 @@
-describe('accountController', function()
+describe('accountController', function() //a test for account controller
 {
     beforeEach(module('app'));
+
     var $controller;
-    var $rootScope;
     var $scope;
+    var authService;
+    var msgService;
 
     var currentUser = undefined;
-    //postMessage: function () {
 
-    //}
+    //instantiating the controller
+    beforeEach(inject(function(_$controller_, _authService_,_msgService_ ){
+        msgService = _msgService_;
+        authService = _authService_;
+        $controller = _$controller_;
+        $scope ={};
+
+    } ));
+
+    //test in the controller
+
+    describe ('$scope.postMessage', function (){
+
+        beforeEach(function () {
+            spyOn(authService, 'currentUser');
+            $controller('accountController', {$scope: $scope, authService: authService, msgService: msgService});
+        });
+
+        it('defaults the scope showNav to be false with no current user', function () {
+            expect(authService.currentUser).toHaveBeenCalled();
+            expect($scope.postMessage).toBeFalsy();
+        });
+    })
+
+
+
+
 
 
 });
